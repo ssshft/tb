69127f82f3023fb303066c7c32908168b18b0a98
sss <songsensen1@163.com>
2026-08-20 14:41:21 +0800

    update

--- src/Gateio/GateioUSTrade.cpp
+++ src/Gateio/GateioUSTrade.cpp
@@ -759,8 +759,8 @@ void GateioUSTradeUnit::add_new_order(const pubsub::TCommand& tcmd) {
         return;
     }
 
-    int pricePrecision = static_cast<int>(1 / info.tickSize);
-    int sizePrecision = static_cast<int>(1 / info.lotSize);
+    int pricePrecision = static_cast<int>(std::llround(-std::log10(info.tickSize)));
+    int sizePrecision = static_cast<int>(std::llround(-std::log10(info.lotSize)));
     std::cout << "pricePrecision: " << pricePrecision << "  sizePrecision: " << sizePrecision << std::endl;
     std::string price_str = priceZero ? "0" : fmt::format("{:.{}f}", pricePrecision, price);
     std::string size_str = fmt::format("{:.{}f}", sizePrecision, sizeSigned);
