d1820126b4747918788b1db3b18f0aadcf1f85a6
sss <songsensen1@163.com>
2026-08-20 13:03:42 +0800

    update

--- src/Gateio/GateioSpotWsTrade.cpp
+++ src/Gateio/GateioSpotWsTrade.cpp
@@ -436,6 +436,8 @@ void GateioSpotWsTradeUnit::handleWsApiResponse(WsPending& pending, const OrderR
 
         rcmd.body.orderResponse.errorId = NoError;
         rcmd.body.orderResponse.updateTime = crypto::getCurrentTime();
+
+        std::cout << "ws api resonse: " << rcmd.getString() << std::endl;
         PUSH_RCMD(rcmd)
     } else if (pending.type == pubsub::CMD_CANCEL_ORDER) {
         crypto::copy_sv_to_char_array(rcmd.body.orderResponse.orderId, fields.id_sv);
