8434da2e395a04849442384a16bafe05b347f593
sss <songsensen1@163.com>
2026-08-23 11:03:35 +0800

    update

--- include/bybit/BybitTradeClient.h
+++ include/bybit/BybitTradeClient.h
@@ -10,14 +10,14 @@ public:
     BybitTradeClient(rapidjson::Value &accountConfig, sm::SecurityManager *smc);
     ~BybitTradeClient();
 
-    void start();
-    void Initial();
-    void add_new_order(pubsub::TCommand &tcmd);
-    void cancel_order(pubsub::TCommand &tcmd);
-    void query_order(pubsub::TCommand &tcmd);
-    void query_account(pubsub::TCommand &tcmd);
-    void query_position(pubsub::TCommand &tcmd);
-    void query_balance(pubsub::TCommand &tcmd);
+    virtual void start();
+    virtual void Initial();
+    virtual void add_new_order(pubsub::TCommand &tcmd);
+    virtual void cancel_order(pubsub::TCommand &tcmd);
+    virtual void query_order(pubsub::TCommand &tcmd);
+    virtual void query_account(pubsub::TCommand &tcmd);
+    virtual void query_position(pubsub::TCommand &tcmd);
+    virtual void query_balance(pubsub::TCommand &tcmd);
 
 protected:
 
